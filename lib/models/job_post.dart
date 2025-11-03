b47d06d43813943a48dbb5db0e34f63f083c86bd
Yonatan L. Ashenafi <54382548+Yonatan235@users.noreply.github.com>
2025-11-03 00:31:56 -0500

    Add JobPost class with JSON serialization methods

--- lib/models/job_post.dart
+++ lib/models/job_post.dart
@@ -1,1 +1,35 @@
+class JobPost {
+  final String title;
+  final String employer;
+  final String notes;
+  final String? imagePath; 
+  final double lat;
+  final double lng;
 
+  JobPost({
+    required this.title,
+    required this.employer,
+    required this.notes,
+    required this.imagePath,
+    required this.lat,
+    required this.lng,
+  });
+
+  Map<String, dynamic> toJson() => {
+        'title': title,
+        'employer': employer,
+        'notes': notes,
+        'imagePath': imagePath,
+        'lat': lat,
+        'lng': lng,
+      };
+
+  static JobPost fromJson(Map<String, dynamic> json) => JobPost(
+        title: json['title'],
+        employer: json['employer'],
+        notes: json['notes'],
+        imagePath: json['imagePath'],
+        lat: json['lat'],
+        lng: json['lng'],
+      );
+}
